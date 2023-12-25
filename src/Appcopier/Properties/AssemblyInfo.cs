5c75b7b0735c7300b8d9eec1e2e206021d469c10
Belim <belim@builtbybel.com>
2023-12-26 00:05:56 +0100

    Update AssemblyInfo.cs

--- src/Appcopier/Properties/AssemblyInfo.cs
+++ src/Appcopier/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.12.119")]
-[assembly: AssemblyFileVersion("0.12.119")]
+[assembly: AssemblyVersion("0.12.125")]
+[assembly: AssemblyFileVersion("0.12.125")]
